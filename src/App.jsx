5880d6aac07ce95d4f91245e5e72318941dccdba
Enayat <enayatansari33@gmail.com>
2025-07-24 23:55:18 +0530

    Initial commit : A todo app

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css";
+import { useState } from "react";
+function App() {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
+  const [editIndex, setEditIndex]=useState(null);
+  function handleClick() {
+    if(!task) return;
+    if (editIndex===null) {
+      setList([...list, task]);
+    }else{
+      const updatedList=list.map((val,index)=> index===editIndex ? task : val);
+      setList(updatedList);
+      setEditIndex(null)
+    }
+    setTask("");
+  }
+
+  function handleDelete(indexToDelete){
+    const newList=list.filter((val,index)=>index!==indexToDelete)
+    setList(newList);
+  }
+
+  function handleEdit(index){
+    setTask(list[index]);
+    setEditIndex(index);
+  }
+
+  return (
+    <div>
+      <h1>My To-do List</h1>
+      <ul>
+        {list.map((value, index) => (
+          <li key={index}>{value}
+          <button onClick={()=>handleDelete(index)}>Delete</button>
+          <button onClick={()=>handleEdit(index)}>{editIndex === null ? "Add the task" : "Update task"}</button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <br />
+      <br />
+      <button onClick={handleClick}>Add the task</button>
+    </div>
+  );
+}
+
+export default App;
